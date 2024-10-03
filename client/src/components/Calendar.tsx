721bc85d62c7fcce4c925440fc7f3ffff3f7391d
Jordan Roberts <jordanroberts333@icloud.com>
2024-10-03 08:37:57 +0100

    feat: current day and date highlighted

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -11,14 +11,25 @@ const Calendar = ({}) => {
         <div className="col-span-7 flex items-center pt-4 text-xl font-sans">
           {monthData.monthName}
         </div>
-        {monthData.days.map((date, dateIndex) => (
+        {monthData.days.map((date, dateIndex) => {
+          const today = new Date();
+          const isToday =
+          date.day === today.getDate() &&
+          date.month === today.getMonth() &&
+          date.year === today.getFullYear();
+          return (
           <div
             key={`${date.year}-${date.month}-${date.day}-${dateIndex}`}
-            className={`${date.isPreviousCarryOver ? 'opacity-30' : 'font-bold'} h-[12vh] flex justify-center items-center`}
+            className={`${date.isPreviousCarryOver ? 'opacity-30' : 'font-bold'} ${isToday && "text-white"} h-[12vh] flex justify-center items-center relative`}
           >
+            {isToday && 
+              <div className="absolute top-1/2 left-1/2 w-3/4 -translate-x-1/2 -translate-y-1/2 bg-red-500 -z-10 rounded-full aspect-square ">
+
+              </div>
+            }
             {date.day}
           </div>
-        ))}
+)})}
       </React.Fragment>
     ))}
   </div>
--- client/src/components/DaysOfWeek.tsx
+++ client/src/components/DaysOfWeek.tsx
@@ -1,9 +1,10 @@
 const DaysOfWeek = ({}) => {
   const days = ["M", "T", "W", "T", "F", "S", "S"];
+  const today = new Date().getDate()
   return (
     <div className="flex px-4">
       {days.map((day, i) => (
-        <div key={i} className="flex-1 flex justify-center">{day}</div>
+        <div key={i} className={`flex-1 flex justify-center ${today === i && "text-red-500 font-bold"}`}>{day}</div>
       ))}
     </div>
   )
